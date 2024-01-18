81ce6ff5ba0d44b06f08eac2fd88e78cd995ec40
Matt <mattcabpro@gmail.com>
2024-01-18 15:16:27 -0500

    functioning +,-,*,/ operations

--- calc.js
+++ calc.js
@@ -1,7 +1,9 @@
 let num1=null;
 let num2=null;
-let operation="";
+let operation=null;
+let computed=false;
 let decpressed=false;
+let negative=false;
 let altkeymap={
     "×" : "*",
     "÷" : "/"
@@ -19,21 +21,30 @@ function getParseable(button){
 }
 
 function operate(){
+    num2=Number(output.textContent);
     switch(operation){
         case "+":
-        break;
+            result=num1+num2;
+            break;
         case "-":
-        break;
+            result=num1-num2;
+            break;
         case "*":
-        break;
+            result=num1*num2;
+            break;
         case "/":
-        break;
-        default:
-        break;
+            result=num1/num2;
+            break;
     }
+    console.log("operation: "+ num1+" "+operation+" "+num2);
+    console.log("result: " + result);
+    output.textContent=result;
+    num1=result;
+    num2=null;
 }
 
-display=document.querySelector(".display");
+output=document.querySelector(".output");
+negsign=document.querySelector("#neg");
 leftbuttons=document.querySelector(".buttonsleft");
 rightbuttons=document.querySelector(".buttonsright")
 
@@ -44,29 +55,81 @@ function assignButtonFunc (button) {
             switch (pressed){
                 case ".":
                     decpressed=true;
-                    console.log("decpressed "+decpressed);
+                    break;
+                case "ON/C":
+                    num1=null;
+                    num2=null;
+                    operator=null;
+                    decpressed=false;
+                    output.textContent="0."
+                    break;
+                case "+/-":
+                    negative=!negative;
+                    negsign.style.color=
+                    negative?"rgb(62, 80, 27)":"rgb(80, 172, 73)";
+                case "+":
+                case "*":
+                case "-":
+                case "/":
+                    if(!computed){
+                        if (num1!==null){
+                            operate();
+                        }
+                        else {
+                            num1=Number(output.textContent);
+                        }
+                        num1=Number(output.textContent);
+                        computed=true;
+                        decpressed=false;
+                    }
+                    else{
+                        num1=Number(output.textContent);
+                    }
+                    operation=pressed;
+                    break;
+                case "=":
+                    if (num1!==null){
+                        operate();
+                    }
+                    num1=null;
+                    operation=null;
+                    computed=true;
+                    decpressed=false;
                     break;
                 default:
-                    console.log(pressed);
+                    console.log(pressed+' not implemented');
+                    //computed=true;
+                    //decpressed=false;
                     break;
             }
         }
         else{
-            if (decpressed){
-                display.textContent+=pressed;
-            }
-            else{
-                if (display.textContent === "0."){
-                    display.textContent=pressed+".";
-                }
-                else{
-                    text=display.textContent;
-                    len=text.length
-                    display.textContent=text.slice(0, len-1)+pressed+".";
-                }
-            }
+            numberInput();
         }
     });
+}
+
+function numberInput(){
+    text=output.textContent;
+    if (computed){
+        text="0.";
+        computed=false;
+    }
+    if(text.length < 13){
+        if (decpressed){
+            text+=pressed;
+        }
+        else{
+            if (text === "0."){
+                text=pressed+".";
+            }
+            else{
+                len=text.length
+                text=text.slice(0, len-1)+pressed+".";
+            }
+        }
+        output.textContent=text;
+    }
 }
 
 leftbuttons.childNodes.forEach(button => assignButtonFunc(button));
@@ -80,4 +143,9 @@ rightbuttons.childNodes.forEach(button => assignButtonFunc(button));
     2) Type in an operation -> store number and store oper
     3) Type in a decimal -> decimal will begin shifting
     4) Type in equals -> same number
-*/
+
+    if a number is computed
+    do not perform an operation until
+    the next input is a number
+    otherwise keep changing operations
+    */
